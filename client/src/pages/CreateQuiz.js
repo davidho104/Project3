2a318a7d466ad8b1b112bbbb6534276c5eed04ce
raywon123 <raymond_won@yahoo.com>
2019-01-24 20:46:55 -0600

    fixing CreateQuiz page

--- client/src/pages/CreateQuiz.js
+++ client/src/pages/CreateQuiz.js
@@ -1,65 +1,116 @@
 import React, { Component } from "react";
-import ManagerPage from "../components/ManagerPage";
 import API from "../utils/API";
-import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 
+import Card from "../components/Card";
+import SearchForm from "../components/SearchForm";
+import { FormBtn } from "../components/Form";
+// import BookDetail from "../components/BookDetail";
 
-var str = "Create Quiz Page"
+var title = "";
+var author = "";
+var description = "";
+var image = "";
+var link = "";
 
 class CreateQuiz extends Component {
   state = {
-    books: [],
-    title: "",
-    author: "",
-    synopsis: ""
+    quizzes: [],
+    search: ""
   };
+
 
   componentDidMount() {
-    this.loadBooks();
+    this.getQuizzes();
   }
 
-  loadBooks = () => {
-    API.getBooks()
-      .then(res =>
-        this.setState({ books: res.data, title: "", author: "", synopsis: "" })
-      )
-      .catch(err => console.log(err));
-  };
-
-  deleteBook = id => {
-    API.deleteBook(id)
-      .then(res => this.loadBooks())
+  // searching Google Book:
+  // title - res.data.items[0].volumeInfo.title
+  // authors - res.data.items[0].volumeInfo.authors);
+  // description - res.data.items[0].volumeInfo.description
+  // image - res.data.items[0].volumeInfo.imageLinks.smallThumbnail
+  // link - res.data.items[0].selfLink);
+  getQuizzes = () => {
+    API.getQuizData()
+      .then(res => {
+        // console.log(res);
+        this.setState({ result: res });
+      })
       .catch(err => console.log(err));
   };
 
   handleInputChange = event => {
-    const { name, value } = event.target;
+    const value = event.target.value;
+    const name = event.target.name;
     this.setState({
       [name]: value
     });
   };
 
-  handleFormSubmit = event => {
-    event.preventDefault();
-    if (this.state.title && this.state.author) {
+
+  // When the form is submitted, search the OMDB API for the value of `this.state.search`
+  // handleFormSubmit = event => {
+  //   event.preventDefault();
+  //   this.searchBooks(this.state.search);
+  // };
+
+
+  handleSaveSubmit = event => {
+    if (title && author) {
       API.saveBook({
-        title: this.state.title,
-        author: this.state.author,
-        synopsis: this.state.synopsis
+        title: title,
+        author: author,
+        description: description,
+        image: image,
+        link: link
       })
-        .then(res => this.loadBooks())
+        .then(res => { console.log(res) })
         .catch(err => console.log(err));
     }
   };
 
   render() {
     return (
-      <Container fluid>
+      <Container>
         <Row>
-          <ManagerPage>
-            {str}
-          </ManagerPage>
+          <Col size="md-8">
+            {this.state.result.map(item => {
+                
+             return <Card key={item.volumeInfo.title} heading={item.volumeInfo.title}>
+
+                <h3>{item.volumeInfo.authors}</h3>
+                <p>{item.volumeInfo.description}</p>
+                <div><img src={item.volumeInfo.imageLinks.thumbnail} alt=""></img></div>
+                <div>
+                  <a href={item.selfLink}>Google Book JSON</a>
+                </div>
+                <div>
+                  <FormBtn
+                    disabled={!(item.volumeInfo.authors && item.volumeInfo.title)}
+                    onClick={() => {
+                      title = item.volumeInfo.title;
+                      author = item.volumeInfo.authors[0];
+                      description = item.volumeInfo.description;
+                      image = item.volumeInfo.imageLinks.thumbnail;
+                      link = item.selfLink
+                      this.handleSaveSubmit();
+                    }}
+                  >
+                    Save
+              </FormBtn>
+                </div>
+              </Card>
+            })}
+          </Col>
+          <Col size="md-4">
+            <Card heading="Search">
+              <SearchForm
+                value={this.state.search}
+                handleInputChange={this.handleInputChange}
+                handleFormSubmit={this.handleFormSubmit}
+              />
+            </Card>
+          </Col>
         </Row>
       </Container>
     );
