b463948d908495157d373187624475756eddbb88
raywon123 <raymond_won@yahoo.com>
2019-01-27 16:54:47 -0600

    clean up old files and update Profile.js

--- client/src/pages/Help.js
+++ client/src/pages/Help.js
@@ -6,7 +6,7 @@ function Help(props) {
   return (
     <div>
       <NavTabsManager />
-      <h1>Contact Page</h1>
+      <h1>Help Page</h1>
       <p>
         Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis molestie urna.
         Aliquam semper ultrices varius. Aliquam faucibus sit amet magna a ultrices. Aenean
