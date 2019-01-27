fd3ab1b51c87fbc123592524e9bd6621e41d15ee
raywon123 <raymond_won@yahoo.com>
2019-01-27 09:25:17 -0600

    add CreateQuiz page - not done yet - first attempt

---
+++ client/src/components/InputForm/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+// This file exports the Input, TextArea, and FormBtn components
+
+export function Input(props) {
+  return (
+    <div className="form-group">
+      <p>Question</p> 
+      <input className="form-control" {...props} />
+      <br></br>
+      <p>Choices (separated by a comma) </p> 
+      <input className="form-control" {...props} />
+      <br></br>
+      <p>Answer</p>
+      <input className="form-control" {...props} />
+    </div>
+  );
+}
+
+export function TextArea(props) {
+  return (
+    <div className="form-group">
+      <textarea className="form-control" rows="20" {...props} />
+    </div>
+  );
+}
+
+export function FormBtn2(props) {
+  return (
+    <button {...props} style={{  marginBottom: 10 }} className="btn btn-success">
+      {props.children}
+    </button>
+  );
+}
