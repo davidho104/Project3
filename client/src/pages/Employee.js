e6ca0186d2b5bb08d1943a9e2c90c29a5474a428
William B. Swift <willbswift@hotmail.com>
2019-01-29 14:21:28 -0600

    now with centering!

--- client/src/pages/Employee.js
+++ client/src/pages/Employee.js
@@ -26,13 +26,13 @@ class Employee extends Component {
     return (
       <Container fluid>
       <NavTabsEmployee />
-        <Row>
-          <Jumbotron>
+      <Jumbotron>
         <h2>Greetings! {this.state.firstname}. Welcome to your first day at the Dunder Mifflin Paper Company.</h2>
         <div class="instructions1">
           <h2>This is your first assignment: click on Quiz tab above.</h2>
         </div>
         </Jumbotron>
+        <Row>
         </Row>
       </Container>
     );
