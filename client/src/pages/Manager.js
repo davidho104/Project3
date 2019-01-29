d236ff3cd683c7f8d721e5c3d2f075b0bec811b8
raywon123 <raymond_won@yahoo.com>
2019-01-29 00:34:37 -0600

    clean up old files

--- client/src/pages/Manager.js
+++ client/src/pages/Manager.js
@@ -4,7 +4,7 @@ import API from "../utils/API";
 // import { Link } from "react-router-dom";
 import Chart from "react-google-charts";
 import { Row, Container } from "../components/Grid";
-import Jumbotron from "../components/Jumbotron";
+// import Jumbotron from "../components/Jumbotron";
 
 
 // Set User Table options
@@ -65,40 +65,7 @@ class Manager extends Component {
       .catch(err => console.log(err));
   };
 
-  loadBooks = () => {
-    API.getBooks()
-      .then(res =>
-        this.setState({ books: res.data, title: "", author: "", synopsis: "" })
-      )
-      .catch(err => console.log(err));
-  };
-
-  deleteBook = id => {
-    API.deleteBook(id)
-      .then(res => this.loadBooks())
-      .catch(err => console.log(err));
-  };
-
-  handleInputChange = event => {
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value
-    });
-  };
-
-  handleFormSubmit = event => {
-    event.preventDefault();
-    if (this.state.title && this.state.author) {
-      API.saveBook({
-        title: this.state.title,
-        author: this.state.author,
-        synopsis: this.state.synopsis
-      })
-        .then(res => this.loadBooks())
-        .catch(err => console.log(err));
-    }
-  };
-
+  
   render() {
     return (
       <Container fluid>
