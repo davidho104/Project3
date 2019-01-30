be1460a0031bb5c3a9519629b67675c1d5e1622b
Laurence Martinez <laurence.martinez14@gmail.com>
2019-01-29 18:18:11 -0600

    horizonal radio buttons in quiz.js.

--- client/src/pages/Quiz.js
+++ client/src/pages/Quiz.js
@@ -22,14 +22,13 @@ function Question(props) {
 // radio buttons in answer box
 function Answer(props) {
   var style = {
-    width: "100%",
     height: 30,
     color: "blue",
   }
   return (
-    <div>
+    <row>
       <button style={style} onClick={() => props.handleClick(props.choice)}>{props.answer}</button>
-    </div>
+    </row>
   )
 }
 
